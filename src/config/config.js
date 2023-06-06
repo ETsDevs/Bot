8ff849e8bf4e36e8eaf481714d25a3228607b013
Erick Furtado <erickf.furtado@outlook.com>
2023-06-05 22:26:47 -0300

    Create Bot

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: 'MTA4ODY1Mzk1MTEwMjA5NTQzMA.GryBYd.bZkC_ogsa62aAibi8XWtzu_xac0nbP-_lOX9Zc',
+    prefix: '=',
+    createRoomCategory: 'Nome da categoria para criar novas salas',
+  };
+  
